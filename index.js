645ad5e4a805c19cf89aa1c650ce524beda5bb83
RSP <217321612+RSP-007@users.noreply.github.com>
2026-02-08 07:41:03 -1200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function makeSound(key){
     switch(key){
 
         case "w":
-            var tom1=new Audio("sounds/tom-1.mp3");
+            var tom1=new Audio("./sounds/tom-1.mp3");
             tom1.play();
             break;
 
@@ -79,3 +79,4 @@ function buttonAnimation(currentKey){
     },100);
 
 }
+
